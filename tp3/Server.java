e2a4868edfb89ac6a424f0022058e45e4d1f41e9
santiago <santiago.g.angeles@gmail.com>
2020-04-23 22:00:36 -0500

    added subscribe and update methods for the object callback

--- tp3/Server.java
+++ tp3/Server.java
@@ -5,9 +5,52 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 public class Server implements Subasta {
 
-    SubastaModelo model;
+    SubastaModelo model = new SubastaModelo();
+
+    private List<Client_Interface> clients = new LinkedList<>();
+
+    public Server() {
+    }
+
+    public boolean suscribe(Client_Interface client) {
+
+        int index = 0;
+        clients.add(client);
+
+        try {
+            for (Client_Interface c : clients) {
+                c.updateSubscription("A new user subscribed for updates");
+                index++;
+            }
+        } catch (Exception e) {
+            clients.remove(index);
+            System.out.println("Unsubscribed client: #" + index);
+        }
+        return true;
+    }
+
+    public boolean updateModel() {
+
+        int index = 0;
+
+        try {
+            for (Client_Interface c : clients) {
+                c.updateModels(model);
+                index++;
+            }
+        } catch (Exception e) {
+            clients.remove(index);
+            System.out.println("Removed Client: " + index);
+        }
+        return true;
+
+    }
 
     public static void main(String args[]) {
 
@@ -28,13 +71,18 @@ public class Server implements Subasta {
 
     @Override
     public SubastaModelo recvModel() throws RemoteException {
-        SubastaModelo m = new SubastaModelo();
-        return m;
+        return model;
     }
 
     @Override
     public void sendModel(SubastaModelo m) throws RemoteException {
         model = m;
+        System.out.println(model.usuarios);
+    }
+
+    @Override
+    public SubastaModelo recvUpdatedModel() throws RemoteException {
+        return model;
     }
 
 }
--- tp3/Subasta.java
+++ tp3/Subasta.java
@@ -2,10 +2,19 @@ package subasta.tp3;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Vector;
 
 public interface Subasta extends Remote {
 
+    boolean suscribe(Client_Interface c) throws RemoteException;
+
+    public boolean updateModel() throws RemoteException;
+
+    // public boolean updateProductList(Vector l) throws RemoteException;
+
     public SubastaModelo recvModel() throws RemoteException;
+
+    public SubastaModelo recvUpdatedModel() throws RemoteException;
 
     public void sendModel(SubastaModelo m) throws RemoteException;
 
